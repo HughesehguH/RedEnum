f53d95fdc778f3da738148882c01bf1a9a096960
Austin <hughesehguh@gmail.com>
2019-12-15 15:22:02 -0500

    add basic command line options

--- RedEnum.py
+++ RedEnum.py
@@ -1,10 +1,18 @@
+import sys, os
+from optparse import OptionParser
+
+
 def main():
+  handleCommandArgs()
 
 
 
+def handleCommandArgs():
+    parser = OptionParser()
+    parser.add_option("-u", "--user", dest="user", help="The reddit account name to be enumerated")
+    parser.add_option("-v", "--verbose", dest="verbose", action="store_true", default=False, help="Turn on verbose mode to give more output")
+    (options, args) = parser.parse_args()
+   
 
-
-
-
-if __name__ == if __name__ == '__main__':
+if __name__ == '__main__':
     main()
